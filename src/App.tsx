7dc8364632400f8c23ff40f3d0904fae4e2bd285
Harry Shapiro <50972797+harrydshapiro@users.noreply.github.com>
2023-01-17 09:54:27 -0500

    save prog

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Project } from './Project';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Project title="fake title" images={[
+        {
+          url: './current-1.png',
+          alt: '',
+          scrollHorizontal: true
+        }
+      ]} description="fooo" dates={{ start: new Date(0), end: new Date(200000)}}/>
     </div>
   );
 }
---
+++ src/plan.md
@@ -0,0 +1,44 @@
+# MVP
+
+## Structure
+
+### **Current**
+
+- Picture from the current project
+- There is basically no difference between this and a project tile, expect for how it's positioned within the greater page
+
+### **About**
+- Headshot
+- Bio
+- Contact (hyperlink to bottom of page)
+
+### **Artists**
+
+- Work gallery
+- Name
+- Bio
+- Link to more about the artists
+
+### **Past Projects**
+- Exhibit gallery
+- Title
+- Dates
+- Description
+
+### **Contact**
+- Email
+- Instagram
+
+## Open Questions
+
+Do artists need a haedshot?
+
+## Deliverables needed
+  - List of artists:
+    - pictures of work
+    - description
+    - external links
+  - List of projects:
+    - pictures of work
+    - description
+    - dates
